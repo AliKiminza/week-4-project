b84d15f32b38348c1de4b0a77a6b2d082624ceb8
alikiminza <ali.kiminza@student.moringaschool.com>
2023-07-07 00:40:36 +0300

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const form = document.querySelector('form');
   const searchInput = document.getElementById('searchByID');
-  const resultContainer = document.getElementById('result');
+  const fetchButton = document.getElementById('fetchButton');
+  const carValueInput = document.getElementById('carValue');
+  const calculateButton = document.getElementById('calculateButton');
+  const resultDiv = document.getElementById('result');
+  const insuranceOptionsDiv = document.getElementById('insuranceOptions');
 
-  form.addEventListener('submit', function (event) {
+  fetchButton.addEventListener('click', handleFetchData);
+  calculateButton.addEventListener('click', handleCalculateCompensation);
+
+  function handleFetchData(event) {
     event.preventDefault();
     const id = searchInput.value.trim();
     if (id !== '') {
@@ -16,37 +22,57 @@ document.addEventListener('DOMContentLoaded', function () {
           }
         })
         .catch(error => {
-          displayErrorMessage('An error occurred while fetching car data.');
+          displayErrorMessage('Error fetching car details');
           console.error(error);
         });
-    } else {
-      displayErrorMessage('Please enter a car ID.');
     }
-  });
+  }
+
+  function handleCalculateCompensation(event) {
+    event.preventDefault();
+    const valueOfCar = parseFloat(carValueInput.value);
+    if (isNaN(valueOfCar)) {
+      displayErrorMessage('Invalid value entered!');
+    } else {
+      const compensationAmount = valueOfCar * 0.8;
+      displayCompensationAmount(compensationAmount);
+    }
+  }
 
   function fetchCarById(id) {
     return fetch('db.json')
       .then(response => response.json())
       .then(data => {
-        const cars = data.cars;
-        return cars.find(car => car.id === id);
+        const car = data.cars.find(car => car.id === id);
+        return car ? car : null;
       });
   }
 
   function displayCarDetails(car) {
     const carDetails = `
       <h3>${car.title}</h3>
-      <p><strong>Claim:</strong> ${car.claim}</p>
+      <p><strong>Claim Type:</strong> ${car.claim}</p>
       <p><strong>Capacity:</strong> ${car.capacity}</p>
       <p><strong>Age Limit:</strong> ${car.age_limit}</p>
-      <p><strong>Compensation Amount:</strong> ${car['compensation amount']}</p>
       <p><strong>Description:</strong> ${car.description}</p>
-      <img src="${car.poster}" alt="${car.title}" style="max-width: 300px;">
+      <img src="${car.poster}" alt="${car.title}" width="300" height="200">
     `;
-    resultContainer.innerHTML = carDetails;
+    resultDiv.innerHTML = carDetails;
   }
 
-  // function displayErrorMessage(message) {
-  //   resultContainer.innerHTML = `<p>${message}</p>`;
-  // }
+  function displayCompensationAmount(amount) {
+    const compensationDetails = `
+      <p>Compensation Amount: ${amount}</p>
+    `;
+    resultDiv.innerHTML += compensationDetails;
+  }
+
+  function displayErrorMessage(message) {
+    const errorMessage = `
+      <p>Error: ${message}</p>
+    `;
+    resultDiv.innerHTML = errorMessage;
+  }
 });
+
+  
